c617ded99450945cdd3f299a58235d87af77231c
mahibala-njit <mb945@njit.edu>
2024-10-18 17:37:49 -0500

    fixed test cases

--- calculator/plugins/menu/__init__.py
+++ calculator/plugins/menu/__init__.py
@@ -1,13 +1,19 @@
 import logging
-from calculator.commands import Command 
+from calculator.commands import Command
 
 # Set up logging for this module
 logger = logging.getLogger(__name__)
 
 class MenuCommand(Command):
     def execute(self):
-        logger.info("Executing MenuCommand.")
-        available_commands = ["add", "subtract", "multiply", "divide", "menu", "exit"]
-        print("Available commands:", ", ".join(available_commands))
-        logger.debug("Available commands: %s", ", ".join(available_commands))
-        return available_commands  # Return list of commands
+        # Define the message for available commands
+        message = "Available commands: add, subtract, multiply, divide, menu, exit"
+        
+        # Print the message to the console
+        print(message)
+        
+        # Log the message for debug purposes
+        logger.info(message)
+        
+        # Return the list of available commands
+        return ["add", "subtract", "multiply", "divide", "menu", "exit"]
